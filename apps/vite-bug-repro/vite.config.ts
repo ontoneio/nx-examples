94a7cdcdfeb0bc803ef85e3bffc6d1649f3834aa
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2024-06-03 18:19:49 -0700

    able to use all commands when not using UnoCSS imports

--- apps/vite-bug-repro/vite.config.ts
+++ apps/vite-bug-repro/vite.config.ts
@@ -1,36 +1,32 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
-import UnoCSS from 'unocss/vite';
-import presetWind from '@unocss/preset-wind';
+import UnoCSS from 'unocss/vite'; 
 
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/vite-bug-repro',
-
+  
   server: {
     port: 4200,
     host: 'localhost',
   },
-
+  
   preview: {
     port: 4300,
     host: 'localhost',
   },
-
+  
   plugins: [
     nxViteTsPaths(),
+    // Uncommenting the below import breaks other projects in the nx graph including commands like 'nx report'
+
     UnoCSS({
       mode: 'shadow-dom',
-      presets: [presetWind()],
+      presets: [],
     }),
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
 
   build: {
     outDir: '../../dist/apps/vite-bug-repro',
